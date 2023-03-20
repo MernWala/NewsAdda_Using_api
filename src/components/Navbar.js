8c909f00f0dea3d6bf9d25d94fd5cc72dd608feb
Shivam Kashyap <shivamkumarkashyap12@gmail.com>
2023-03-20 11:36:57 +0530

    bug fix with bootstrap 5 link

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -7,9 +7,11 @@ export default class Navbar extends Component {
             <nav className="navbar navbar-dark bg-dark navbar-expand-lg px-3">
                 <div className="container-fluid">
                     <a className="navbar-brand" href="/">News Adda</a>
-                    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                        <span className="navbar-toggler-icon"></span>
+
+                    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                        <span class="navbar-toggler-icon"></span>
                     </button>
+
                     <div className="collapse navbar-collapse" id="navbarSupportedContent">
                         <ul className="navbar-nav me-auto mb-2 mb-lg-0">
                             <li className="nav-item">
